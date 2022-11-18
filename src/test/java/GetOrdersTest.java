2b86e1895f732ac75f7dd0c679136123d9b71b13
Golos Ilya <ilyagolos8@gmail.com>
2022-11-18 15:48:57 +0300

    fix tests 2

--- src/test/java/GetOrdersTest.java
+++ src/test/java/GetOrdersTest.java
@@ -6,6 +6,7 @@ import io.restassured.response.ValidatableResponse;
 import org.hamcrest.MatcherAssert;
 import org.junit.Before;
 import org.junit.Test;
+import org.hamcrest.Matchers;
 
 import java.util.ArrayList;
 
